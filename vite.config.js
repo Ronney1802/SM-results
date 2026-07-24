f3a8b9954329cebfb5d102b9e17bcdabad6ff512
Ronney1802 <ronney1802@gmail.com>
2026-07-24 16:46:55 +0530

    added note about base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,20 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SM-results/", // 👈 important for GitHub Pages
+  base: "/SM-results/", // important for GitHub Pages when working with a subdirectory into vite project.
 });
+
+/* 
+  Note: 
+  -> The base option is set to "/SM-results/" to ensure that the application works correctly when deployed 
+  to GitHub Pages under the specified subdirectory. 
+  -> This is necessary because GitHub Pages serves the application from a subdirectory rather than the root of the domain.
+  -> By setting the base option, all asset paths and routing will be correctly resolved relative to the subdirectory (SM-results), 
+  it happens because the application is not hosted at the root of the domain, but rather in a subdirectory named SM-results. 
+  -> This ensures that all asset paths and routing will be correctly resolved relative to the subdirectory,
+  allowing the application to function properly when accessed via the GitHub Pages URL.
+  -> This problem doesn't come when working with CRA (Create React App) because CRA automatically handles the base path for GitHub Pages deployments,
+  but in Vite, you need to manually set the base option in the vite.config.js file to ensure correct routing and asset loading.
+  -> So in vite you must add base option in vite.config.js file to ensure correct routing and asset loading when deploying to GitHub Pages
+  and not using root or custom domain instead subdirectory.
+ */

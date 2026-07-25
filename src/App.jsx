e1ce6f508fe03485e8667c0ab909d9241c86774e
Ronney1802 <ronney1802@gmail.com>
2026-07-25 13:24:53 +0530

    lint fix(semicolon)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css'
-import Home from './components/pages/Home'
+import "./App.css";
+import Home from "./components/pages/Home";
 
 function App() {
-
   return (
     <div className="app-conatiner">
-     <Home />
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

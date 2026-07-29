df495cad84e8c1404bf18f9a95162a08c743dfed
Ronney1802 <ronney1802@gmail.com>
2026-07-30 01:06:34 +0530

    updated package after installing cross-env to control the local, git and railway deployment all together

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SM-results/", // important for GitHub Pages when working with a subdirectory into vite project.
+  // base: "/SM-results/", // important for GitHub Pages when working with a subdirectory into vite project.
+  /* Base not needed anymore because we are using cross env :- 
+  (updated the below code into scripts in package.json after installing "cross-env": "^10.1.0",)
+   "build:gh": "cross-env VITE_BASE_PATH=/SM-results/ vite build",
+   "deploy": "npm run build:gh && gh-pages -d dist" */
 });
 
 /* 
